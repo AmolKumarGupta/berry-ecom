da4f714f54e022d615fab85d295562f777467425
amol <amlo1232001@gmail.com>
2022-03-04 23:53:41 +0530

    [dev] Add responsive navbar🎉🎉🎉🎉

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./resources/js/index.js ***!
+  \*******************************/
+document.getElementById('navBtn').addEventListener('click', function () {
+  document.getElementById('navItems').classList.toggle('hidden');
+});
+/******/ })()
+;
